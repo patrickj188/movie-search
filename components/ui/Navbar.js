4cc9209c48c015ee1b7962bdaf94e97bd350736d
patrickj188 <patrickj188@gmail.com>
2022-06-06 16:17:43 -0700

    cleaned up search code

--- components/ui/Navbar.js
+++ components/ui/Navbar.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import Link from 'next/link'
 import style from "./Navbar.module.css"
-import LoginPage from "./loginPage"
+import LoginPage from "./loginPage.js"
 
 
 
