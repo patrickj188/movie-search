a846a8b2ee7bdfae403e05cbb14a54846c740b1b
patrickj188 <patrickj188@gmail.com>
2022-06-07 16:08:29 -0700

    fixed context issue

---
+++ pages/users.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useSession } from "next-auth/react";
+function User() {
+  const { data: session } = useSession();
+  if (!session) {
+    return <p>You are not logged in.</p>;
+  }
+  return <h1>you are logged in</h1>;
+
+}
+export default User;
